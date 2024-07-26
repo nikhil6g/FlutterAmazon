134a0da50f12f5043e845bfb91ca8fcb3730a4c4
Nikhil Gupta <nikhil6g2003@gmail.com>
2024-07-26 16:02:58 +0530

    created get all the products and delete products api

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -23,4 +23,28 @@ adminRoute.post('/admin/add-product',admin,async (req,res) => {
     }
 })
 
+
+//get all the products
+adminRoute.get('/admin/get-products',admin,async (req,res)=>{
+    try{
+        const products = await Product.find({})
+        res.json(products)
+    }catch(e){
+        res.status(500).json({err : e.message})
+    }
+})
+
+//delete product
+adminRoute.post('/admin/delete-product',admin,async (req,res) =>{
+    try{
+        const {id} = req.body
+
+        let product = await Product.findByIdAndDelete(id);
+
+        res.json(product)
+    }catch(e){
+        res.status(500).json({err : e.message})
+    }
+})
+
 module.exports = adminRoute
